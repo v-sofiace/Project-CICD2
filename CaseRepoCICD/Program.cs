1a0bbd2b213f21eec445bcc47d593caf2522a728
sofia <sofiacerdas10@gmail.com>
2025-03-24 18:12:16 +0000

    Upgrading project

--- CaseRepoCICD/Program.cs
+++ CaseRepoCICD/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.Azure.Functions.Worker.Builder;
+/* using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -9,5 +9,51 @@ builder.ConfigureFunctionsWebApplication();
 // builder.Services
 //     .AddApplicationInsightsTelemetryWorkerService()
 //     .ConfigureFunctionsApplicationInsights();
+
+builder.Build().Run(); */
+
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Storage.Queues;
+using func_WarehouseBoxSys.Services;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Configuration;
+using Azure.Identity;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+string azStorageQueueConn = builder.Configuration["ShippoNotificationQueueConn"];
+
+builder.ConfigureFunctionsWebApplication();
+
+// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+//builder.Services.AddApplicationInsightsTelemetryWorkerService(options =>
+//{
+//    options.ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+//});
+
+// Configure logging
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddApplicationInsights();
+});
+
+//// Register Azure Queue Storage
+builder.Services.AddAzureClients(clients =>
+{
+    clients.AddQueueServiceClient(azStorageQueueConn);
+
+});
+
+// Register AzureQueueHelper
+builder.Services.AddScoped<IAzureQueueHelper, AzureQueueHelper>();
+
+// Register HttpClient
+builder.Services.AddHttpClient();
+builder.Services.AddTransient<IMailService, MailService>();
 
 builder.Build().Run();
